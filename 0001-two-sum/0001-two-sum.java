066945da6ac1fa38c4e9182621a90f57aa19fa0e
Abhishek Patil <abhishekbpatil16@gmail.com>
2024-04-01 01:39:53 +0530

    Time: 2 ms (97.83%), Space: 44.7 MB (72.55%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -4,6 +4,7 @@ class Solution {
         // element : index
         Map<Integer,Integer> indexMap = new HashMap<Integer,Integer>();
         
+        // One-pass Hash Table method
         for(int i=0; i<nums.length; i++) {
             Integer index = indexMap.get(target-nums[i]);
             if(index != null) {
